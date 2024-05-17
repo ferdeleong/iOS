e8fc9bde18bad1fa16a8323d09aba0c18e014b37
Fernanda De León <fernandadeleon@Fernandas-MacBook-Pro.local>
2024-05-17 15:55:12 -0600

    Add chart examples

---
+++ darkmode2/darkmode2/darkmode2App.swift
@@ -0,0 +1,17 @@
+//
+//  darkmode2App.swift
+//  darkmode2
+//
+//  Created by Fernanda De León on 16/05/24.
+//
+
+import SwiftUI
+
+@main
+struct darkmode2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
